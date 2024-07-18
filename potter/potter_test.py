81693d3737872c24729af9a0df64b6844b8ad949
Victor Moreno <victor.moreno@cabify.com>
2024-07-18 21:21:07 +0200

    test!: bookmark broken edge case

--- potter/potter_test.py
+++ potter/potter_test.py
@@ -7,18 +7,20 @@ def test_basics():
     assert calculate_price([1, 1]) == 16
     assert calculate_price([1, 2]) == 15.2
     assert calculate_price([1, 2, 3]) == 21.6
-    assert calculate_price([1, 2, 3, 4]) == 27.2
-    assert calculate_price([1, 2, 3, 4, 5]) == 32
+    assert calculate_price([1, 2, 3, 4]) == 25.6
+    assert calculate_price([1, 2, 3, 4, 5]) == 30
 
 
 def test_two_different_and_some_repeated_books():
     assert calculate_price([1, 2, 2]) == 23.2
-    # assert calculate_price([1, 2, 2, 2]) == 31.2
     assert calculate_price([1, 2, 3, 3, 3]) == 37.6
 
 
 def test_sets():
     assert calculate_price([1, 2, 1, 2]) == 30.4
+    assert calculate_price([1, 2, 1, 2, 3]) == 37.6
+    assert calculate_price([1, 2, 2, 3, 4, 5]) == 38
+    assert calculate_price([1, 1, 2, 2, 3, 3, 4, 5]) == 51.2
 
 
 def test_no_sets():
